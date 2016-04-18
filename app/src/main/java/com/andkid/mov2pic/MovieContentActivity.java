91f3afbd7903085fcd452a47e07ad7f6eddd6cc9
yuguan.chen <yuguan.chen@tcl.com>
2016-04-18 18:37:46 +0800

    show movie title and summary, will add them to scroll view later

--- app/src/main/java/com/andkid/mov2pic/MovieContentActivity.java
+++ app/src/main/java/com/andkid/mov2pic/MovieContentActivity.java
@@ -3,8 +3,6 @@ package com.andkid.mov2pic;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.util.Log;
 import android.widget.TextView;
 
 import com.andkid.mov2pic.callback.MovieContentCallback;
@@ -22,18 +20,15 @@ public class MovieContentActivity extends Activity {
     TextView mSummaryView;
 
     @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        Log.i("cyg", "oncreate dou buzou ?");
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.list_movie_content);
-        Log.i("cyg", "buxinxie");
 
         mTitleView = (TextView) findViewById(R.id.title);
         mSummaryView = (TextView) findViewById(R.id.summary);
 
         Intent intent = getIntent();
         String uri = intent.getStringExtra(WebSites.PARAMETER_URI);
-        Log.i("cyg", "start get");
         OkHttpUtils.get()
                 .url(WebSites.MOVIE_CONTENT_URL)
                 .addParams(WebSites.PARAMETER_URI, uri)
@@ -42,12 +37,11 @@ public class MovieContentActivity extends Activity {
 
                     @Override
                     public void onError(Call call, Exception e) {
-                        Log.i("cyg", "title = " + e);
+
                     }
 
                     @Override
                     public void onResponse(MovieContent response) {
-                        Log.i("cyg", "title = " + response.title);
                         mMovieContent = response;
                         initContentView();
                     }
@@ -56,7 +50,13 @@ public class MovieContentActivity extends Activity {
 
     void initContentView() {
         mTitleView.setText(mMovieContent.title);
-        mSummaryView.setText(mMovieContent.summary.toString());
+        StringBuilder summary = new StringBuilder();
+        if(mMovieContent.summary != null) {
+            for(String s : mMovieContent.summary) {
+                summary.append(s.trim());
+            }
+        }
+        mSummaryView.setText(summary);
     }
 
 
