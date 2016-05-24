22ad29b2929de6e68e84a500ff501d65e0e34158
AndKid <851097647@qq.com>
2016-05-24 21:25:31 +0800

    move server from alibaba to baidu

--- app/src/main/java/com/andkid/mov2pic/WebSites.java
+++ app/src/main/java/com/andkid/mov2pic/WebSites.java
@@ -5,10 +5,14 @@ package com.andkid.mov2pic;
  */
 public class WebSites {
     public static final String DOMAIN = "http://www.k165.com/";
-    public static final String MOVIE_LIST_URL = "http://120.76.115.38/";
-    public static final String MOVIE_CONTENT_URL = "http://120.76.115.38/content";
-    public static final String MOVIE_BACKGROUND_URL = "http://120.76.115.38/background";
-    public static final String MOVIE_BG_HDPI_URL = "http://120.76.115.38/bg_hdpi";
+//    public static final String MOVIE_LIST_URL = "http://120.76.115.38/";
+//    public static final String MOVIE_CONTENT_URL = "http://120.76.115.38/content";
+//    public static final String MOVIE_BACKGROUND_URL = "http://120.76.115.38/background";
+//    public static final String MOVIE_BG_HDPI_URL = "http://120.76.115.38/bg_hdpi";
+    public static final String MOVIE_LIST_URL = "http://mov2pic.duapp.com/";
+    public static final String MOVIE_CONTENT_URL = "http://mov2pic.duapp.com/content";
+    public static final String MOVIE_BACKGROUND_URL = "http://mov2pic.duapp.com/background";
+    public static final String MOVIE_BG_HDPI_URL = "http://mov2pic.duapp.com/bg_hdpi";
     public static final String PARAMETER_URI = "uri";
     public static final String PARAMETER_URL = "url";
 }
